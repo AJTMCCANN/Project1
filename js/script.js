acffb2589dcebbb2224e09db5ec6a666b5f58003
AJTMCCANN <adam.jt.mccann@gmail.com>
2017-01-31 14:46:14 -0700

    Added *.txt to .gitignore

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var recycled_quotes = []
+
+function getRandomIndex() {
+	var index = Math.floor(Math.random() * quotes.length);
+	return index
+}
+
+function getRandomQuote() {
+	var index = getRandomIndex()
+	quote = quotes.pop(index);
+	recycled_quotes.unshift(quote);
+	if (quote === undefined) {
+		quotes = recycled_quotes;			// quotes get put in the recycled_quotes array (defined at top of script) when they are popped
+		recycled_quotes = [];				// this 'if' block runs when the array runs out of quotes from 'quotes.pop(index)'
+		index = getRandomIndex();			// it resets the 'quotes' array for another runthrough
+		quote = quotes.pop(index);
+		recycled_quotes.unshift(quote);
+		return quote
+	}
+	return quote
+}
+
+function printQuote() {
+	var quote = getRandomQuote();
+	var new_quote = "<p class=\"quote\">" + quote[0] + "</p>"
+	var new_source = "<p class=\"source\">" + quote[1] + "<span class=\"citation\">" + quote[2] + "</span><span class = \"year\">" + quote[3] + "</span><span class=\"category\">" + quote[4] + "</span></p>"
+	document.getElementById('quote-box').innerHTML = new_quote + new_source
+
+	if ($(".citation").text() === "") { $(".citation").addClass('hideable') };		//these three lines avoid a bunch of trailing commmas that would be
+	if ($(".year").text() === "") { $(".year").addClass('hideable') };				//inserted due to the ':before' pseudoelements' css styling if a quote
+	if ($(".category").text() === "") { $(".category").addClass('hideable') };		//was missing one of these three properties
+
+	$("body").css("background-color", getRandomColor());	// randomizes the background color
+
+}
+
+function getRandomColor() {
+	var hex = Math.floor(Math.random() * 0xFFFFFF);
+	return "#" + ("000000" + hex.toString(16)).substr(-6);
+}
+
+setInterval(printQuote, 30000);	 //if the user does nothing a new quote is displayed every 30 seconds
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);  // event listener to respond to "Show another quote" button clicks
+document.getElementById('loadQuote').click(); 								// when user clicks anywhere on the button, the "printQuote" function is called
---
+++ js/subset_of_cleaned_up_quotes.js
@@ -0,0 +1,264 @@
+var quotes = [
+  [
+    "A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.",
+    "Lana Turner", "", "", ""
+  ],
+  [
+    "People who think they know everything are a great annoyance to those of us who do.",
+    "Isaac Asimov", "", "", ""
+  ],
+  [
+    "The universe is a big place, perhaps the biggest.",
+    "Kurt Vonnegut", "", "", ""
+  ],
+  [
+    "Legend: A lie that has attained the dignity of age.",
+    "H. L. Mencken", "", "", ""
+  ],
+  [
+    "Marriage is a wonderful institution, but who would want to live in an institution?",
+    "H. L. Mencken", "", "", ""
+  ],
+  [
+    "An idealist is one who, on noticing that a rose smells better than a cabbage, concludes that it will also make better soup.",
+    "H. L. Mencken", "", "", ""
+  ],
+  [
+    "A cynic is a man who, when he smells flowers, looks around for a coffin.",
+    "H. L. Mencken", "", "", ""
+  ],
+  [
+    "I never comment on referees and I'm not going to break the habit of a lifetime for that prat.",
+    "Ron Atkinson", "", "", ""
+  ],
+  [
+    "Solutions are not the answer.",
+    "Richard Nixon, former U.S. President", "", "", ""
+  ],
+  [
+    "Every tooth in a man's head is more valuable than a diamond.",
+    "Miguel de Cervantes", "Don Quixote", "1605", "Teeth"
+  ],
+  [
+    "The man with a toothache thinks everyone happy whose teeth are sound.",
+    "George Bernard Shaw", "", "", "Teeth"
+  ],
+  [
+    "Liberty is the right to do what I like; license, the right to do what you like.",
+    "Bertrand Russell", "", "", ""
+  ],
+  [
+    "It is preoccupation with possessions, more than anything else, that prevents us from living freely and nobly.",
+    "Bertrand Russell", "", "", ""
+  ],
+  [
+    "Freedom in general may be defined as the absence of obstacles to the realization of desires.",
+    "Bertrand Russell", "", "", ""
+  ],
+  [
+    "Sufficiently advanced incompetence is indistinguishable from malice.", "Unknown", "", "", ""
+  ],
+  [
+    "Learning is not compulsory. Neither is survival.",
+    "W. Edwards Deming", "", "", ""
+  ],
+  [
+    "To iterate is human, to recurse divine.",
+    "L. Peter Deutsch", "", "", ""
+  ],
+  [
+    "The competent programmer is fully aware of the strictly limited size of his own skull; therefore he approaches the programming task in full humility, and among other things he avoids clever tricks like the plague.",
+    "Edsger Dijkstra", "", "", ""
+  ],
+  [
+    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.",
+    "Eagleson's law", "", "", ""
+  ],
+  [
+    "I have always found that plans are useless, but planning is indispensable.",
+    "Dwight Eisenhower", "", "", ""
+  ],
+  [
+    "The inside of a computer is as dumb as hell but it goes like mad!",
+    "Richard Feynman", "", "", ""
+  ],
+  [
+    "You can know the name of a bird in all the languages of the world, but when you're finished, you'll know absolutely nothing whatever about the bird... So let's look at the bird and see what it's doing. That's what counts. I learned very early the difference between knowing the name of something and knowing something.",
+    "Richard Feynman", "", "", ""
+  ],
+  [
+    "It always takes longer than you expect, even when you take into account Hofstadter's Law.",
+    "Hofstadter's Law", "", "", ""
+  ],
+  [
+    "It's not at all important to get it right the first time. It's vitally important to get it right the last time.",
+    "Andrew Hunt and David Thomas", "", "", ""
+  ],
+  [
+    "Before software can be reusable it first has to be usable.",
+    "Ralph Johnson", "", "", ""
+  ],
+  [
+    "Without stress nothing would move.",
+    "Bill Langley", "", "", ""
+  ],
+  [
+    "Good code is its own best documentation. As you're about to add a comment, ask yourself, 'How can I improve the code so that this comment isn't needed?' Improve the code and then document it to make it even clearer.",
+    "Steve McConnell", "Code Complete", "", ""
+  ],
+  [
+    "Alas, to wear the mantle of Galileo it is not enough that you be persecuted by an unkind establishment, you must also be right.",
+    "Bob Park", "", "", ""
+  ],
+  [
+    "Even if you're on the right track, you'll get run over if you just sit there.",
+    "Will Rogers", "", "", ""
+  ],
+  [
+    "Most of you are familiar with the virtues of a programmer. There are three, of course: laziness, impatience, and hubris.",
+    "Larry Wall", "", "", ""
+  ],
+  [
+    "We all agree on the necessity of compromise. We just can't agree on when it's necessary to compromise.",
+    "Larry Wall", "", "", ""
+  ],
+  [
+    "If builders built buildings the way programmers wrote programs, then the first woodpecker that came along would destroy civilisation.",
+    "Gerald Weinberg", "", "", ""
+  ],
+  [
+    "If you think your management doesn't know what it's doing or that your organisation turns out low-quality software crap that embarrasses you, then leave.",
+    "Edward Yourdon", "Rise and Resurrection of the American Programmer", "", ""
+  ],
+  [
+    "Some people, when confronted with a problem, think 'I know, I'll use regular expressions.' Now they have two problems.",
+    "Jamie Zawinski", "", "", ""
+  ],
+  [
+    "'What our ancestors would really be thinking, if they were alive today, is: Why is it so dark in here?'",
+    "Terry Pratchett", "Pyramids", "", ""
+  ],
+  [
+    "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.",
+    "Terry Pratchett", "Diggers", "", ""
+  ],
+  [
+    "Give a man a fire and he's warm for the day, but set fire to him and he's warm for the rest of his life.",
+    "Terry Pratchett", "Jingo", "", "Funny"
+  ],
+  [
+    "It is said that your life flashes before your eyes just before you die. That is true, it's called Life.",
+    "Terry Pratchett", "The Last Continent", "", "Funny"
+  ],
+  [
+    "In ancient times cats were worshipped as gods; they have not forgotten this.",
+    "Terry Pratchett", "", "", "Funny"
+  ],
+  [
+    "It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.",
+    "Terry Pratchett", "", "", "Funny"
+  ],
+  [
+    "In the beginning there was nothing, which exploded.",
+    "Terry Pratchett", "Lords and Ladies", "", "Funny"
+  ],
+  [
+    "Wisdom comes from experience. Experience is often a result of lack of wisdom.",
+    "Terry Pratchett", "", "", ""
+  ],
+  [
+    "It would seem that you have no useful skill or talent whatsoever,' he said. 'Have you thought of going into teaching?",
+    "Terry Pratchett", "Mort", "", "Funny"
+  ],
+  [
+    "A distributed system is one in which the failure of a computer you didn't even know existed can render your own computer unusable.",
+    "Leslie Lamport", "", "", ""
+  ],
+  [
+    "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+    "John F. Woods", "", "", ""
+  ],
+  [
+    "Civilization began the first time an angry person cast a word instead of a rock.",
+    "Sigmund Freud", "", "", ""
+  ],
+  [
+    "All murderers are punished unless they kill in large numbers and to the sound of trumpets.",
+    "Voltaire", "", "", ""
+  ],
+  [
+    "The injury we do and the one we suffer are not weighed in the same scale.",
+    "Aesop", "", "", ""
+  ],
+  [
+    "If English was good enough for Jesus, it's good enough for them Mexicans.",
+    "Texas politician", "", "", ""
+  ],
+  [
+    "Prohibition goes beyond reason in that it attempts to control a man's appetite through legislation. A prohibition law strikes a blow at the very principles this country was founded upon.",
+    "Abraham Lincoln", "", "", ""
+  ],
+  [
+    "Problems cannot be solved at the same level of awareness that created them.",
+    "Albert Einstein", "", "", ""
+  ],
+  [
+    "We can often do more for other men by trying to correct our own faults than by trying to correct theirs.",
+    "Francois Fenelon", "", "", ""
+  ],
+  [
+    "Everywhere is walking distance if you have the time.",
+    "Steven Wright", "", "", ""
+  ],
+  [
+    "Money often costs too much.",
+    "Ralph Waldo Emerson", "", "", ""
+  ],
+  [
+    "We need either less corruption or more chance to participate in it.", "Unknown", "", "", ""
+  ],
+  [
+    "USA Today has come out with a new survey: Apparently three out of four people make up 75 percent of the population.",
+    "David Letterman.", "", "", ""
+  ],
+  [
+    "In ancient times they had no statistics so they had to fall back on lies.",
+    "Stephen Leacock", "", "", ""
+  ],
+  [
+    "Reality is merely an illusion, albeit a very persistent one.",
+    "Albert Einstein", "", "", ""
+  ],
+  [
+    "Sometimes, the best answer is a more interesting question",
+    "Terry Pratchett", "", "", ""
+  ],
+  [
+    "Whatever is begun in anger ends in shame.",
+    "Benjamin Franklin", "", "", ""
+  ],
+  [
+    "Half this game is ninety percent mental.",
+    "Yogi Berra", "", "", ""
+  ],
+  [
+    "If you were plowing a field, which would you rather use? Two strong oxen or 1024 chickens?",
+    "Seymour Cray", "", "", ""
+  ],
+  [
+    "The man who does not read good books has no advantage over the man who cannot read them.",
+    "Mark Twain", "", "", ""
+  ],
+  [
+    "He has all the virtues I dislike and none of the vices I admire.",
+    "Winston Churchill", "", "", ""
+  ],
+  [
+    "Let's not bring age into this discussion. I will not let my opponent's youth and inexperience become an issue.",
+    "Ronald Reagan on Walter Mondale", "", "", ""
+  ],
+  [
+    "Properly read, the Bible is the most potent force for atheism ever conceived.",
+    "Isaac Asimov", "", "", ""
+  ]
+]
